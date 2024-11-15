e79b948851b3a47ee10a5b25fe0f2663724ff654
Philippe Carle <philippe@moooood.app>
2024-11-15 10:59:59 -0700

    Some refactoring

--- api/src/Entity/Entry.php
+++ api/src/Entity/Entry.php
@@ -3,30 +3,56 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
-use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\{Get, GetCollection, Post};
 use App\Repository\EntryRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Bridge\Doctrine\Types\UuidType;
+use Symfony\Component\Uid\Uuid;
 
 #[ORM\Entity(repositoryClass: EntryRepository::class)]
-#[ApiResource(operations: [
-    new Post()
-])]
+#[ORM\Index(name: 'idx_created_at', columns: ['created_at'])]
+#[ORM\Index(name: 'idx_content_fulltext', columns: ['content'], flags: ['fulltext'])] // PostgreSQL-specific
+#[ApiResource(
+    operations: [
+        new Get(),
+        new GetCollection(normalizationContext: ['groups' => [Entry::SERIALIZATION_GROUP_READ_COLLECTION]]),
+        new Post(),
+    ],
+    normalizationContext: ['groups' => [Entry::SERIALIZATION_GROUP_READ_ITEM]],
+    denormalizationContext: ['groups' => [Entry::SERIALIZATION_GROUP_WRITE]],
+)]
 class Entry
 {
-    public const GROUP_NOTIFICATION = 'notification';
+    public const SERIALIZATION_GROUP_SNS = 'entry:sns';
+    public const SERIALIZATION_GROUP_WRITE = 'entry:write';
+    public const SERIALIZATION_GROUP_READ_ITEM = 'entry:read:item';
+    public const SERIALIZATION_GROUP_READ_COLLECTION = 'entry:read:collection';
 
     #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+    #[ORM\Column(type: UuidType::NAME, unique: true)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: 'doctrine.uuid_generator')]
+    #[Groups([self::SERIALIZATION_GROUP_READ_ITEM])]
+    private Uuid $id;
 
     #[ORM\Column(type: Types::TEXT)]
-    #[Groups([self::GROUP_NOTIFICATION])]
+    #[Groups([self::SERIALIZATION_GROUP_WRITE, self::SERIALIZATION_GROUP_READ_ITEM, self::SERIALIZATION_GROUP_SNS])]
     private ?string $content = null;
 
-    public function getId(): ?int
+    #[ORM\Column(type: Types::DATETIMETZ_IMMUTABLE)]
+    #[Gedmo\Timestampable(on: 'create')]
+    #[Groups([self::SERIALIZATION_GROUP_READ_ITEM])]
+    public ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column(type: Types::DATETIMETZ_IMMUTABLE)]
+    #[Gedmo\Timestampable]
+    #[Groups([self::SERIALIZATION_GROUP_READ_ITEM])]
+    public ?\DateTimeImmutable $updatedAt = null;
+
+    public function getId(): Uuid
     {
         return $this->id;
     }
--- api/src/EventListener/EntryWriteListener.php
+++ api/src/EventListener/EntryWriteListener.php
@@ -4,27 +4,15 @@ namespace App\EventListener;
 
 use ApiPlatform\Symfony\EventListener\EventPriorities;
 use App\Entity\Entry;
-use Psr\Log\LoggerInterface;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use App\Notifier\EntrySnsNotifier;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Event\ViewEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
-use Symfony\Component\Notifier\Bridge\AmazonSns\AmazonSnsOptions;
-use Symfony\Component\Notifier\Exception\TransportExceptionInterface;
-use Symfony\Component\Notifier\Message\ChatMessage;
-use Symfony\Component\Notifier\TexterInterface;
-use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
-use Symfony\Component\Serializer\SerializerInterface;
 
 final class EntryWriteListener implements EventSubscriberInterface
 {
-    public function __construct(
-        private readonly TexterInterface $texter,
-        private readonly SerializerInterface $serializer,
-        private readonly ParameterBagInterface $parameterBag,
-        private readonly LoggerInterface $logger,
-    ) {}
+    public function __construct(private readonly EntrySnsNotifier $notifier) {}
 
     public static function getSubscribedEvents()
     {
@@ -38,23 +26,10 @@ final class EntryWriteListener implements EventSubscriberInterface
         $entry = $event->getControllerResult();
         $method = $event->getRequest()->getMethod();
 
-        if (!$entry  || Request::METHOD_POST !== $method) {
+        if (!$entry instanceof Entry || Request::METHOD_POST !== $method) {
             return;
         }
 
-        $payload = $this->serializer->serialize($entry, 'jsonld', [
-            AbstractNormalizer::GROUPS => [Entry::GROUP_NOTIFICATION],
-        ]);
-
-        $options = new AmazonSnsOptions($this->parameterBag->get('new_entry_sns_arn'));
-        // $options->messageStructure('json');
-
-        $message = new ChatMessage($payload, $options);
-
-        try {
-            $this->texter->send($message);
-        } catch (TransportExceptionInterface $e) {
-            $this->logger->error('An error occurred while sending the SNS notification: '.$e->getMessage());
-        }
+        $this->notifier->notify($entry);
     }
 }
