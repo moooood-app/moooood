6dd0a5d08a9c0fcabe51a1442bdd748231dc2ae0
Philippe Carle <philippe.carle@pixieset.com>
2024-11-18 23:09:29 -0700

    Fix tests

--- api/src/Repository/EntryMetadataRepository.php
+++ api/src/Repository/EntryMetadataRepository.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Repository;
 
+use App\Entity\Entry;
 use App\Entity\EntryMetadata;
+use App\Message\ProcessorOutputMessage;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -16,5 +18,16 @@ class EntryMetadataRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, EntryMetadata::class);
+    }
+
+    public function createMetadataFromProcessorOutput(Entry $entry, ProcessorOutputMessage $processorOutputMessage): void
+    {
+        $metadata = new EntryMetadata();
+        $metadata->setEntry($entry);
+        $metadata->setProcessor($processorOutputMessage->getProcessor());
+        $metadata->setMetadata($processorOutputMessage->getResult());
+
+        $this->getEntityManager()->persist($metadata);
+        $this->getEntityManager()->flush();
     }
 }
