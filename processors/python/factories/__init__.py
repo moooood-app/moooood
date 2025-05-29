619721820f3f9de6482149fc37203ee8a666ae30
Philippe Carle <philippe.carle@pixieset.com>
2025-05-29 12:47:29 -0600

    Remove legacy processors

--- processors/python/factories/__init__.py
+++
@@ -1,1 +0,0 @@
-from .service_factory import ServiceFactory
--- processors/python/protocols/processor.py
+++
@@ -1,6 +0,0 @@
-from typing import Union, List, Dict, Any, Protocol
-
-class Processor(Protocol):
-    def process(self, entry: str) -> Union[Dict[str, Any], List[Any]]:
-        """Processes entry and returns the metadata."""
-        pass
